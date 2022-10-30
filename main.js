b7bfaa27d544ebe3c4f51904255081de6a57039c
eaCe <webmaster@slaps.de>
2022-10-30 07:45:21 +0100

    sort urls

--- main.js
+++ main.js
@@ -100,6 +100,20 @@ ipcMain.handle('scanSite', async (event, url) => {
         });
     });
 
+    /**
+     * sort by type
+     */
+    urls.sort((a, b) => {
+        const typeA = a.type.toUpperCase();
+        const typeB = b.type.toUpperCase();
+        return (typeA > typeB) ? -1 : (typeA < typeB) ? 1 : 0;
+    });
+
+    /**
+     * sort by host
+     */
+    urls.sort((a, b) => Number(b.same_host) - Number(a.same_host));
+
     // await page.on('response', response => {
     // const url = new URL(response.url());
     // const host = url.host.replace('www.', '');
